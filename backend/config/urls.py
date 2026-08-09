f8fa23543cc354ca0b72bb53958368f7212daca8
SharoMonk <lekan.akinremi@gmail.com>
2026-08-09 16:02:26 +0100

    feat: add POS token authentication endpoint

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,5 +1,10 @@
-from django.urls import include, path
 from django.contrib import admin
+from django.urls import include, path
+from rest_framework.authtoken.views import obtain_auth_token
 from commerce.api import api
 
-urlpatterns = [path('admin/', admin.site.urls), path('api/', include(api.urls))]
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/auth/token/', obtain_auth_token, name='api-token'),
+    path('api/', include(api.urls)),
+]
